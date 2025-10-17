82adae8f61a385f8a4f1267f9f03a29e9d3e3e93
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-10-17 13:02:58 +0100

    cree CustomerService

--- customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customer_service/CustomerServiceApplication.java
@@ -1,7 +1,15 @@
 package com.example.customer_service;
 
+import com.example.customer_service.entities.Customer;
+import com.example.customer_service.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +17,30 @@ public class CustomerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return atgs->{
+			customerRepository.save(Customer.builder()
+					.name("Mohamed").email("med@gmail.com")
+					.build());
+			customerRepository.save(Customer.builder()
+					.name("Imane").email("imane@gmail.com")
+					.build());
+			customerRepository.save(Customer.builder()
+					.name("Yassine").email("yassine@gmail.com")
+					.build());
+			customerRepository.findAll().forEach(c->{
+				System.out.println("======================");
+				System.out.println(c.getId());
+				System.out.println(c.getName());
+				System.out.println(c.getEmail());
+				System.out.println("=======================");
+			});
 
+
+
+		};
+
+
+	}
 }
